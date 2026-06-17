6291398424b231202f07e24d4f7ca1f2ed200cda
Tusti Sindigi <tustiisindigi@gmail.com>
2026-06-17 21:55:10 +0530

    merge nodes in between zeros sol 2

--- linked-list/merge-nodes-in-between-zeros.c
+++ linked-list/merge-nodes-in-between-zeros.c
@@ -8,36 +8,32 @@ struct ListNode
  
 struct ListNode* mergeNodes(struct ListNode* head) 
 {
-    struct ListNode* p=head;
-    struct ListNode* n, *nn;
+    struct ListNode* p=head->next;
+    struct ListNode* n=head;
 
     int sum=0;
 
-    while(p!=NULL)
+    while(p)
     {
-        if(sum!=0 && p->val==0)
+        if(p->val==0)
         {
-            struct ListNode* temp=malloc(sizeof(struct ListNode));
-            temp->val=sum;
-            temp->next=NULL;
+            n->val=sum;
+            n->next=p;
+            
+            if(p->next==NULL)
+            {
+                n->next=NULL;
+                break;
+            }
 
-            if(n==NULL)
-            {
-                n=temp;
-                nn=n;
-            }
-            else
-            {
-                n->next=temp;
-                n=n->next;
-            }
-                
+            n=n->next;
             sum=0;
+            
         }
         else
             sum+=p->val;
 
         p=p->next;
     }
-    return nn;
+    return head;
 }
